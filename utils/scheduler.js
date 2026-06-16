2ea3b78475e5fb6e3cafb7f2df47cdb933cb3c66
Neha Yednurwar <yednurwarneha@gmail.com>
2026-06-16 10:13:46 +0530

    10:15

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -153,7 +153,7 @@ const runAlertJob = async () => {
 };
 
 // Runs every day at 10:00 AM IST
-cron.schedule('05 10 * * *', runAlertJob, {
+cron.schedule('15 10 * * *', runAlertJob, {
   timezone: 'Asia/Kolkata'
 });
 
