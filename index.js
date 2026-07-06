402a83fbad8e99f9bc16077fb9f93ebc0ac31a06
Neha Yednurwar <yednurwarneha@gmail.com>
2026-07-06 16:00:57 +0530

    ........

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use('/api/compliances', require('./routes/compliance'));
 app.use('/api/users', require('./routes/user'));
 app.use('/api/alertlogs', require('./routes/alertLog'));
 app.use('/api/recordassignments', require('./routes/recordAssignment'));
-app.use("/api/safety-meetings", require("./routes/safetyMeetings"));
+app.use("/api/safety-meetings", require("./routes/SafetyMeetings"));
 
 app.get('/', (req, res) => res.send('CompliTrack API running'));
 
