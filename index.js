403b2ed0ed7391bcb1655d74da144f05e867863f
Neha Yednurwar <yednurwarneha@gmail.com>
2026-08-17 15:29:46 +0530

    .....

--- index.js
+++ index.js
@@ -29,8 +29,3 @@ mongoose.connect(process.env.MONGO_URI)
   })
   .catch(err => console.error('MongoDB error:', err));
 
-
-
-
-
-  // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjZhMmY5OTM3YzU4YzI3ZDUyN2ExMTcxZCIsImlhdCI6MTc4MTY5MDIyOCwiZXhwIjoxNzgyMjk1MDI4fQ.IqTns46cdPdvJhQz_sK6CRW-6CLlBA6xOTTOEcAZAkU
